768ea498593cb94ace544a5a255cd1ec9cd878f1
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-03 23:08:46 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,57 @@
 require_relative './part_1_solution.rb'
+require "pry"
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupons.each do |coupon_item|
+    #check the coupon against each item in the cart
+    cart.each do |cart_item|
+      #check whether it's the same item.
+      if cart_item[:item] == coupon_item[:item]
+        #check to see if there are more or equal amount of item than it says on the coupon
+        if cart_item[:count] >= coupon_item[:num]
+          #the new count of the item now equals the item left after coupon
+          cart_item[:count] = cart_item[:count] - coupon_item[:num]
+          #makes a new array index with all the keys for the couponed item
+          cart[cart.length] = {
+            :item => cart_item[:item] + " W/COUPON",
+            :price => coupon_item[:cost] / coupon_item[:num],
+            :clearance => cart_item[:clearance],
+            :count => coupon_item[:num]
+            }
+        end
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  # goes into each grocery_item in the cart
+  cart.each do |grocery_item|
+    # checks if it is on clearance and applies clearance
+    if grocery_item[:clearance] == true
+      grocery_item[:price] = (grocery_item[:price] * 0.80).round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  final_total = 0
+  #consolidates cart
+  consolidated_cart = consolidate_cart(cart)
+  # variable for the applied coupons using consolidated_cart
+  pre_clearance_total = apply_coupons(consolidated_cart, coupons)
+  #variable for appliying clearances after applying coupons
+  ready_for_checkout_total = apply_clearance(pre_clearance_total)
+  # runs through each item after applying coupons and clearances that collects final_total
+  ready_for_checkout_total.each do |grocery_item|
+    item_total = grocery_item[:price] * grocery_item[:count]
+    final_total = final_total + item_total
+  end
+  #extra 10% off if less than $100
+  if final_total > 100.00
+    final_total = final_total * 0.90
+  end
+  final_total
 end
