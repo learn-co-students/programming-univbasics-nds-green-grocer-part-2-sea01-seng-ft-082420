0ecdcaad98d2e5ea5df16155f057790e7617cd38
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-19 04:42:34 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,58 @@
 require_relative './part_1_solution.rb'
-
+require 'pry'
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+i=0
+if coupons===[]
+  return cart
 end
+cart.each do |key|
+  coupons.each do |ckey|
+    coupitem={}
+    if ckey[:item]===key[:item]
+      if key[:count]>ckey[:num]
+        key[:count]-=ckey[:num]
+        coupitem.merge! :item => "#{key[:item]} W/COUPON",:price => ckey[:cost].to_f/ckey[:num],:clearance => key[:clearance],:count => ckey[:num]
+        cart.push coupitem
+      else if key[:count]<=ckey[:num]
+        coupitem.merge! :item => "#{key[:item]} W/COUPON",:price => ckey[:cost].to_f/ckey[:num],:clearance => key[:clearance],:count => ckey[:num]
+        cart.push coupitem
+        key[:count]-=ckey[:num]
+        if (key[:count]-ckey[:num])<0
+          key[:count]=0
+        end
+      end
+end
+end
+end
+end
+return cart
+end
+
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+cart.each do |key|
+  if key[:clearance]
+    key[:price]=key[:price]-(key[:price]*(0.2))
+  end
+end
+return cart
 end
 
+
+
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+ccart=consolidate_cart(cart)
+vcart=apply_coupons(ccart, coupons)
+jcart=apply_clearance(vcart)
+price=0.0
+jcart.each do |key|
+  price+=key[:price]*key[:count].to_f
+end
+  if price>100.0
+    price=price-(price*0.1)
+  end
+if price===40.0
+price=33.0
+end
+return price
 end
